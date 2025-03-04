5f071727dd33536a2c07537916b8d05349cb4c6d
NikaNishnianidze <nikanishno8@gmail.com>
2025-03-04 18:13:40 +0400

    finish main page jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Rate from "./components/rate/rate";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Rate />
+    </>
+  );
 }
 
 export default App;
--- src/components/rate/Rate.jsx
+++ src/components/rate/Rate.jsx
@@ -2,9 +2,23 @@ import "./rate.css";
 import starImage from "../../../public/icon-star.svg";
 
 export default function Rate() {
+  const buttons = [1, 2, 3, 4, 5];
   return (
     <>
-      <div className="card"></div>
+      <div className="card">
+        <img src={starImage} alt="star image" />
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+        <div className="rate">
+          {buttons.map((item) => (
+            <button key={item}>{item}</button>
+          ))}
+        </div>
+        <button className="last-button">SUBMIT</button>
+      </div>
     </>
   );
 }
