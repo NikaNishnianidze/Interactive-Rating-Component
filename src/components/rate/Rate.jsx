7c2ef5ffa4190d3229fe87c9183af66dc61d24b4
NikaNishnianidze <nikanishno8@gmail.com>
2025-03-04 18:05:17 +0400

    starter setub

---
+++ src/components/rate/Rate.jsx
@@ -0,0 +1,10 @@
+import "./rate.css";
+import starImage from "../../../public/icon-star.svg";
+
+export default function Rate() {
+  return (
+    <>
+      <div className="card"></div>
+    </>
+  );
+}
