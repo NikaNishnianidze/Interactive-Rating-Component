95783b760e2189b3d75767efd79a93105a84dac0
NikaNishnianidze <nikanishno8@gmail.com>
2025-03-04 18:23:30 +0400

    finish styling desktop css

--- src/components/rate/Rate.jsx
+++ src/components/rate/Rate.jsx
@@ -6,7 +6,10 @@ export default function Rate() {
   return (
     <>
       <div className="card">
-        <img src={starImage} alt="star image" />
+        <div className="image">
+          <img src={starImage} alt="star image" />
+        </div>
+
         <h1>How did we do?</h1>
         <p>
           Please let us know how we did with your support request. All feedback
